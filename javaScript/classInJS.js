1774cc57eb27487dbb68154ea38a4d0be552b422
MrTypeError <sanugta8@gmail.com>
2022-09-28 17:48:50 +0530

    changes in Animations and little of java

--- javaScript/classInJS.js
+++ javaScript/classInJS.js
@@ -29,26 +29,26 @@
 
 // let pencil=new Product("Pencil",20,2,"PE1");
 
-const Product1=class Product{
-    constructor(itemName,itemPrice,discount,itemCode){
-        this.itemName=itemName; 
-        this.itemPrice=itemPrice;
-        this.discount=discount;
-        this.itemCode=itemCode;
-    }
-    // getter
-    get getDiscount(){
-        return(this.discount);
-    }
-    // setter
-    set setDiscount(value){
-        this.discount=value;
-    }
-    // this is a methor but basically it will allow you to do the same functionality as getter allows you.
-    // but when you are calling a function you have to give () but in setter you don't.
-    DiscountValue(){
-        return this.discount*this.itemPrice/100;
-    }
-};
+// const Product1=class Product{
+//     constructor(itemName,itemPrice,discount,itemCode){
+//         this.itemName=itemName; 
+//         this.itemPrice=itemPrice;
+//         this.discount=discount;
+//         this.itemCode=itemCode;
+//     }
+//     getter
+//     get getDiscount(){
+//         return(this.discount);
+//     }
+//     setter
+//     set setDiscount(value){
+//         this.discount=value;
+//     }
+//     this is a methor but basically it will allow you to do the same functionality as getter allows you.
+//     but when you are calling a function you have to give () but in setter you don't.
+//     DiscountValue(){
+//         return this.discount*this.itemPrice/100;
+//     }
+// };
 
-let chair=new Product1("Chair",499,10,"CH1");
+// let chair=new Product1("Chair",499,10,"CH1");
